42416839cf27a25085b5aa67a4147cf9e8b32050
nguyenthithanhsuong <22521268@gm.uit.edu.vn>
2026-05-26 08:47:11 +0700

    restore app.js

--- app.js
+++ app.js
@@ -4,6 +4,11 @@ const port = process.env.PORT || 3001;
 
 app.get("/", (req, res) => res.type('html').send(html));
 
+const server = app.listen(port, () => console.log(`Example app listening on port ${port}!`));
+
+server.keepAliveTimeout = 120 * 1000;
+server.headersTimeout = 120 * 1000;
+
 const html = `
 <!DOCTYPE html>
 <html>
@@ -53,13 +58,3 @@ const html = `
     </section>
   </body>
 </html>
-`
-
-if (require.main === module) {
-  const server = app.listen(port, () => console.log(`Example app listening on port ${port}!`));
-
-  server.keepAliveTimeout = 120 * 1000;
-  server.headersTimeout = 120 * 1000;
-}
-
-module.exports = app;
